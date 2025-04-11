d9d5bd7cdcc14aeccbc83f873983eb5ffe406442
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-04-11 15:13:03 +0700

    [RED] Add skeleton chat message

---
+++ src/main/java/id/ac/ui/cs/advprog/chat/model/ChatMessage.java
@@ -0,0 +1,23 @@
+package com.adpro.chat.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long senderId;
+    private Long receiverId;
+    private String content;
+    private String status;
+    private LocalDateTime timestamp;
+}
