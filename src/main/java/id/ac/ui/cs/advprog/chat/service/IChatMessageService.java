aa0e3e3a200fb6a3c40ca3c26a0d91a289e8022a
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-05-02 12:26:50 +0700

    [GREEN] added interface for chat service

---
+++ src/main/java/id/ac/ui/cs/advprog/chat/service/IChatMessageService.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.chat.service;
+
+import id.ac.ui.cs.advprog.chat.model.ChatMessage;
+import java.util.List;
+import java.util.Optional;
+
+public interface IChatMessageService {
+
+    /** Create */
+    ChatMessage sendMessage(ChatMessage message);
+
+    /** Read single */
+    Optional<ChatMessage> getMessage(Long messageId);
+
+    /** Read all */
+    List<ChatMessage> getAllMessages();
+
+    /** Update */
+    Optional<ChatMessage> editMessage(Long messageId, String newContent);
+
+    /** “Delete” (soft delete) */
+    Optional<ChatMessage> deleteMessage(Long messageId);
+}
