f1314525b71f2b5bbd4b67da50da69c6c3f72c45
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-05-09 13:40:51 +0700

    [RED] added ChatApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/chat/ChatApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/chat/ChatApplicationTests.java
@@ -2,12 +2,22 @@ package id.ac.ui.cs.advprog.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ChatApplicationTests {
+class ChatApplicationTest {
 
     @Test
     void contextLoads() {
+        ChatApplication.main(new String[] {});
     }
 
+    @Test
+    void testCorsConfigurerBean() {
+        ChatApplication app = new ChatApplication();
+        WebMvcConfigurer configurer = app.corsConfigurer();
+        assertNotNull(configurer); // basic check
+    }
 }
