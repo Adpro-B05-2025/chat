c1313ef0df3a415c40b7bb00952a9562b23b189f
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-05-19 14:03:41 +0700

    [RED] added chat room repository

---
+++ src/main/java/id/ac/ui/cs/advprog/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.chat.repository;
+
+import id.ac.ui.cs.advprog.chat.model.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByPacilianIdAndDoctorId(Long pacilianId, Long doctorId);
+}
