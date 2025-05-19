0acf78111a4f82bd5429630b1ab6eba993c36ae1
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-05-19 13:52:35 +0700

    [GREEN] added WebSocketConfig

---
+++ src/main/java/id/ac/ui/cs/advprog/chat/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry cfg) {
+        cfg.enableSimpleBroker("/topic");              // in-memory broker
+        cfg.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry reg) {
+        reg.addEndpoint("/ws-chat")                    // handshake URL
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
